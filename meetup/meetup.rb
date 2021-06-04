af048f8d1f99cb3ae0585291483a2192068715a6
Iain Diamond <iain.diamond@gmail.com>
2021-06-04 11:36:57 +0100

    Refactor Meetup

--- meetup/meetup.rb
+++ meetup/meetup.rb
@@ -8,24 +8,23 @@ class Meetup
   end
 
   def day(weekday, which)
-    wday = Time.new(@year, @month).wday
-    @days_in_month = Date.new(@year, @month, -1).day
-    days_of_month = (1..@days_in_month).zip((DAYSOFWEEK * 6).drop(wday))
-    matching_days = days_of_month.select { |_, day| day == weekday }
+    start_date = Date.new(@year, @month)
+    end_date = Date.new(@year, @month, -1)
+    matching_days = (start_date..end_date).select(&:"#{weekday}?")
     case which
     when :first
-      date, = matching_days.first
+      date = matching_days.first
     when :second
-      date, = matching_days[1]
+      date = matching_days[1]
     when :third
-      date, = matching_days[2]
+      date = matching_days[2]
     when :fourth
-      date, = matching_days[3]
+      date = matching_days[3]
     when :last
-      date, = matching_days.last
+      date = matching_days.last
     when :teenth
-      date, = matching_days.find { |num,| num > 12 }
+      date = matching_days.find { |match| match.day > 12 }
     end
-    Date.new(@year, @month, date)
+    Date.new(@year, @month, date.day)
   end
 end
