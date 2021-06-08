788c2e776d00d70701a51e16c77f6e09eaaa655f
Iain Diamond <iain.diamond@gmail.com>
2021-06-08 14:21:44 +0100

    Refactor solution

--- kindergarten-garden/kindergarten_garden.rb
+++ kindergarten-garden/kindergarten_garden.rb
@@ -1,15 +1,15 @@
 class Garden
-  STUDENTS = %w[alice bob charlie david eve fred ginny harriet ileana joseph kincaid larry].freeze
+  STUDENTS = %w[alice bob charlie david eve
+                fred ginny harriet ileana
+                joseph kincaid larry].freeze
   PLANTS = %i[clover grass radishes violets].freeze
   LOOKUP = 'CGRV'.chars.zip(PLANTS).to_h
   def initialize(rows, students = STUDENTS)
+    boxes = rows.lines.map { |row| row.scan(/../) }.transpose
     students = students.map(&:downcase).sort
     students.each.with_index do |student, index|
       define_singleton_method(student) do
-        index *= 2
-        row1, row2 = rows.lines
-        plants = row1[index..index + 1] + row2[index..index + 1]
-        plants.chars.map { |plant| LOOKUP.fetch(plant) }
+        boxes[index].join.chars.map(&LOOKUP)
       end
     end
   end
